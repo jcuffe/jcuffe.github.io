7b19c3121297b6073c50bc8f651391a3f878842f
Jared Cuffe <jcuffe@gmail.com>
2019-01-18 10:16:18 -0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0dbe3a56c67a9fb8d040.js"
+    "url": "webpack-runtime-e95c5f83db759e5cfda4.js"
   },
   {
     "url": "app-bcb16b751fdaeb197e51.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "22dad05f3e2f623b1443aa1f2cc10351"
+    "revision": "cc547692f37ce47153b673c0f8ecc7df"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
