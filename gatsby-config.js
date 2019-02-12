30a15d200a1bf8d02dabb946c1e61fbf1886fdef
Jared Cuffe <jcuffe@gmail.com>
2019-02-12 13:31:13 -0800

    Implements repository fetching for portfolio.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,27 @@ module.exports = {
       options: {
         pathToConfigModule: "src/util/typography"
       }
+    },
+    {
+      resolve: "gatsby-source-github-api",
+      options: {
+        token: "3d25feacf4b62c0ad8358875cc6206796eba18fe", // Heavily restricted for convenience
+        graphQLQuery: `
+          query { 
+            viewer{
+              pinnedRepositories(first:5) {
+                edges {
+                  node{ 
+                    name
+                    url
+                    description
+                  }
+                }
+              }
+            }
+          } 
+        `
+      }
     }
   ]
 };
