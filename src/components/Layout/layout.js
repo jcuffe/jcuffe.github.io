1419ddba41c604b3f4774fdab0c8b3403b7b37a1
Jared Cuffe <jcuffe@gmail.com>
2019-02-20 11:51:28 -0800

    Styles layout and replaces static data with query.

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,18 +1,37 @@
 import React from "react";
-import { Link } from "gatsby";
+import { StaticQuery, Link, graphql } from "gatsby";
 import styles from "./layout.module.css";
 
-export default ({ children }) => (
-  <div className={styles.layout}>
-    <header className={styles.header}>
-      <Link to={"/"}>
-        <h3>Jared Cuffe</h3>
-      </Link>
-      <span className={styles.navigation}>
-        <Link to={"/about/"}>About</Link>
-        <Link to={"/portfolio/"}>Portfolio</Link>
-      </span>
-    </header>
-    {children}
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
+const render = (data, children) => (
+  <div>
+    <div className={styles.gradient} />
+    <div className={styles.layout}>
+      <header className={styles.header}>
+        <Link to={"/"}>
+          <h3>{data.site.siteMetadata.title}</h3>
+        </Link>
+        <span className={styles.navigation}>
+          <Link to={"/"}>About</Link>
+          <Link to={"/blog/"}>Blog</Link>
+          <Link to={"/portfolio/"}>Portfolio</Link>
+          <a href="https://resume.creddle.io/resume/xsgwo141h1">Resume</a>
+        </span>
+      </header>
+      {children}
+    </div>
   </div>
+);
+
+export default ({ children }) => (
+  <StaticQuery query={query} render={data => render(data, children)} />
 );
