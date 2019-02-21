1a5bc92413a87fd149ee714d1685f6cec6ca30db
Jared Cuffe <jcuffe@gmail.com>
2019-02-21 11:55:49 -0800

    Styles post template.

--- src/templates/post/post.js
+++ src/templates/post/post.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../../components/layout/layout";
+import styles from "./post.module.css";
 
 export default ({ data }) => {
   const { frontmatter, html } = data.markdownRemark;
   return (
     <Layout>
-      <div>
+      <div className={styles.post}>
         <h1>{frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </div>
